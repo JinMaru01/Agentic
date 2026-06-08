f9548cdb8e059158b5ee1b053687ec2d96eb00f4
Darachin Kong <darachin.kong@wingbank.com.kh>
2026-06-08 09:50:54 +0000

    init dev

--- retriever/embedding.py
+++
@@ -1,6 +0,0 @@
-from documents import documents
-from sentence_transformers import SentenceTransformer
-
-embedding_model = SentenceTransformer("all-MiniLM-L6-v2")
-
-embeddings = embedding_model.encode(documents)
